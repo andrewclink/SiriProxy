e541d74c9eb38db47927654d378105070c4fe55e
Andrew Clink <andrew@elementsmi.com>
2013-02-01 09:25:12 -0700

    Reorganize a bit

--- plugins/siriproxy-lights/lib/siriproxy-lights.rb
+++ plugins/siriproxy-lights/lib/siriproxy-lights.rb
@@ -43,97 +43,6 @@ class SiriProxy::Plugin::Lights < SiriProxy::Plugin
     #ctx.usb_close                 
   end
 
-  filter "PersonSearchCompleted" do |object|
-    puts "[Info - PersonSearchCompleted] Object:"
-    results = object["properties"]["results"]
-    if results.length < 1
-      puts " -> No Results"
-    else
-      contact = results[0]["properties"]
-      @firstName = contact["firstName"]
-      @lastName  = contact["lastName"]
-    end
-    
-    puts "Name: #{@firstName} #{@lastName} (wants_person = #{@wants_person})"
-    
-    if @wants_person
-      manager.callback.call(contact)
-      false
-    else
-      nil
-    end
-  end
-  
-  def set_screen_blank(isoff)
-
-    say "Ok, I'll turn the screen #{isoff ? "off" : "on"}"
-
-    if isoff
-      fork do 
-        exec 'screen-off'
-      end
-    else
-      File.unlink("/tmp/screen-off-lock") rescue nil
-    end 
-  end
-
-  listen_for /turn (off|on) (?:the )?screen/i do |onoff|
-    set_screen_blank(!(onoff =~ /on/i))
-    request_completed
-  end
-
-  listen_for /turn (?:the )?screen (off|on)/i do |onoff|
-    set_screen_blank(!(onoff =~ /on/i))
-    request_completed
-  end
-  
-  listen_for /reset (?:the )?fans ?(?:please)/i do
-    system('killall fancontrol')
-    system('service fancontrol start')
-    say "Ok, sorry about the noise!"
-    request_completed
-  end
-  
-  listen_for /person/i do
-    
-    f = Fiber.current
-    @current_fiber = f
-    @wants_person = true
-    
-    s = SiriPersonSearch.new("PersonSearch", "com.apple.ace.contact")
-    s.properties["me"] = "true"
-    # s.properties["name"] = "Alex"
-    s.properties["scope"] = "Local"
-    s.make_root(last_ref_id)
-    
-    puts "====== PersonSearch Request ======="
-    pp s.to_hash
-    puts " "
-    
-    # manager.respond "Gotcha @{tts#\e\\pause=500\\\e\\rate=90\\} person"
-
-    add_views = SiriAddViews.new
-    add_views.temporary = true
-    add_views.make_root(last_ref_id)
-    utterance = SiriAssistantUtteranceView.new("Gotcha @{tts#\e\\pause=500\\\e\\rate=90\\} person")
-    add_views.views << utterance
-
-    #you can also do "send_object object, target: :guzzoni" in order to send an object to guzzoni
-    send_object add_views #send_object takes a hash or a SiriObject object
-
-
-    send_object s, target: :iphone
-    manager.set_callback do |text|
-      say "Looks like you're #{@firstName} #{@lastName}"
-      f.resume(text)
-    end
-
-    Fiber.yield
-
-    say "Done"
-    request_completed
-    
-  end
 
 #pragma mark - Lights
 
@@ -170,6 +79,11 @@ class SiriProxy::Plugin::Lights < SiriProxy::Plugin
     h = word_to_integer(hour) + (period =~ /pm/ ? 12 : 0)
     m = word_to_integer(minute) 
     Time.new now.year, now.month, now.day, h, m
+  end
+
+  listen_for /test lights/i do
+    say "Lights available, context: #{@ftdi}"
+    request_completed
   end
 
   listen_for /turn (on|off) the lights at ([1-9]|1[0-2])\:(\d\d)?\s*(am|pm)?/i do |state, hour, minute, period|
@@ -286,10 +200,7 @@ class SiriProxy::Plugin::Lights < SiriProxy::Plugin
     request_completed
   end
 
-  listen_for /test lights/i do
-    say "Lights available, context: #{@ftdi}"
-    request_completed
-  end
+
   
   def handle_lights(state, where)
     begin
@@ -346,21 +257,55 @@ class SiriProxy::Plugin::Lights < SiriProxy::Plugin
     handle_lights(state, where)
   end
 
-  listen_for /turn the(bedroom)? lights? (on|off)/i do |where, state|
+  listen_for(/turn the(bedroom)? lights? (on|off)/i) do |where, state|
     handle_lights(state, where)
   end
 
-  listen_for Regexp.new(/blink the lights?/i) do
+  listen_for(/blink the lights?/i) do
     say "Blinking!"
     request_completed
 
-    10.times do 
-      @ftdi.write_data([0xff])
-      sleep(0.5)
-      @ftdi.write_data([0x00])
-      sleep(0.25)
+    fork do
+      10.times do 
+        @ftdi.write_data([0xff])
+        sleep(0.5)
+        @ftdi.write_data([0x00])
+        sleep(0.25)
+      end
     end
-    
+  end
+  
+
+  # Other useful stuff that I haven't properly dealt with
+
+  def set_screen_blank(isoff)
+
+    say "Ok, I'll turn the screen #{isoff ? "off" : "on"}"
+
+    if isoff
+      fork do 
+        exec 'screen-off'
+      end
+    else
+      File.unlink("/tmp/screen-off-lock") rescue nil
+    end 
+  end
+
+  listen_for /turn (off|on) (?:the )?screen/i do |onoff|
+    set_screen_blank(!(onoff =~ /on/i))
+    request_completed
+  end
+
+  listen_for /turn (?:the )?screen (off|on)/i do |onoff|
+    set_screen_blank(!(onoff =~ /on/i))
+    request_completed
+  end
+  
+  listen_for /reset (?:the )?fans ?(?:please)/i do
+    system('killall fancontrol')
+    system('service fancontrol start')
+    say "Ok, sorry about the noise!"
+    request_completed
   end
   
 
